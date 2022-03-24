311d1fabf0250e5672ef16ad34e5b1b5e22f8ef1
Gama <94679178+GamaGenta@users.noreply.github.com>
2022-03-24 17:20:17 +0100

    Update Arbeit.php

--- Arbeit.php
+++ Arbeit.php
@@ -5,7 +5,14 @@ class Arbeit
     private $titel;
     private $startzeitpunkt;
     private $endzeitpunkt;
-
+    
+    public function __construct($titel, $startzeitpunkt, $endzeitpunkt)
+    {
+        $this->titel = $titel;
+        $this->startzeitpunkt = $startzeitpunkt;
+        $thie->endzeitpunkt = $endzeitpunkt;
+    }
+        
     public function getTitel() {
         return $this->startzeitpunkt;
     }
