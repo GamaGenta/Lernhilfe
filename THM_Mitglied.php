c37d23e1591c227026b035b3ef4285b775e4617e
Philipp <philipp_rockel@web.de>
2022-03-27 14:11:50 +0200

    Getter eingefügt und variablen auf private gestellt

--- THM_Mitglied.php
+++ THM_Mitglied.php
@@ -2,21 +2,43 @@
 
 class THM_Mitglied
 {
-    var $ID;
-    var $vorname;
-    var $name;
-    var $rolle;
-    var $matrikelnummer;
-    var $mitarbeiternummer;
+    private var $ID;
+    private var $vorname;
+    private var $name;
+    private var $rolle;
 
-    public function __construct($ID, $vorname, $name, $rolle, $matrikelnummer, $mitarbeiternummer)
+
+    public function getID()
+    {
+        return $this->ID;
+    }
+
+
+    public function getVorname()
+    {
+        return $this->vorname;
+    }
+
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+
+    public function getRolle()
+    {
+        return $this->rolle;
+    }
+
+
+    public function __construct($ID, $vorname, $name, $rolle)
     {
         $this->ID = $ID;
         $this->vorname = $vorname;
         $this->name = $name;
         $this->rolle = $rolle;
-        $this->matrikelnummer = $matrikelnummer;
-        $this->mitarbeiternummer = $mitarbeiternummer;
+
     }
 
 
