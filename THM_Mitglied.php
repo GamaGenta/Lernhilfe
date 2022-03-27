51677a741f2096588e3cafdce48d13aa809e7c9f
Philipp <philipp_rockel@web.de>
2022-03-27 14:13:39 +0200

    Semantik geändert

--- THM_Mitglied.php
+++ THM_Mitglied.php
@@ -7,6 +7,14 @@ class THM_Mitglied
     private var $name;
     private var $rolle;
 
+    public function __construct($ID, $vorname, $name, $rolle)
+    {
+        $this->ID = $ID;
+        $this->vorname = $vorname;
+        $this->name = $name;
+        $this->rolle = $rolle;
+
+    }
 
     public function getID()
     {
@@ -29,16 +37,6 @@ class THM_Mitglied
     public function getRolle()
     {
         return $this->rolle;
-    }
-
-
-    public function __construct($ID, $vorname, $name, $rolle)
-    {
-        $this->ID = $ID;
-        $this->vorname = $vorname;
-        $this->name = $name;
-        $this->rolle = $rolle;
-
     }
 
 
