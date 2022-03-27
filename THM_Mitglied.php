30b07e46cca37f169a670e3e86172ddf87f04cdd
Philipp <philipp_rockel@web.de>
2022-03-27 11:15:32 +0200

    Constructor eingefügt

--- THM_Mitglied.php
+++ THM_Mitglied.php
@@ -10,72 +10,13 @@ class THM_Mitglied
     var $Mitarbeiternummer;
 
 
-    public function getID()
-    {
-        return $this->ID;
-    }
-
-
-    public function setID($ID)
+    public function __construct($ID, $Vorname, $Name, $Rolle, $Matrikelnummer, $Mitarbeiternummer)
     {
         $this->ID = $ID;
-    }
-
-    public function getVorname()
-    {
-        return $this->Vorname;
-    }
-
-
-    public function setVorname($Vorname)
-    {
         $this->Vorname = $Vorname;
-    }
-
-
-    public function getName()
-    {
-        return $this->Name;
-    }
-
-    public function setName($Name)
-    {
         $this->Name = $Name;
-    }
-
-
-    public function getRolle()
-    {
-        return $this->Rolle;
-    }
-
-
-    public function setRolle($Rolle)
-    {
         $this->Rolle = $Rolle;
-    }
-
-
-    public function getMatrikelnummer()
-    {
-        return $this->Matrikelnummer;
-    }
-
-
-    public function setMatrikelnummer($Matrikelnummer)
-    {
         $this->Matrikelnummer = $Matrikelnummer;
-    }
-
-
-    public function getMitarbeiternummer()
-    {
-        return $this->Mitarbeiternummer;
-    }
-
-  
-    public function setMitarbeiternummer($Mitarbeiternummer)
-    {
         $this->Mitarbeiternummer = $Mitarbeiternummer;
     }
 
