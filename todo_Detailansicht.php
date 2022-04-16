c7094c5fc0538e7d352a18a3d3ae9f7103c87bbd
bmsru2 <98160933+bmsru2@users.noreply.github.com>
2022-04-16 13:50:21 +0200

    Add files via upload

---
+++ todo_Detailansicht.php
@@ -0,0 +1,46 @@
+<?php
+include_once '../other/dbconn.php';
+?>
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToDo Anlegen - Lernhilfe</title>
+    <link rel="stylesheet" href="style_todo_Detailansicht.css">
+</head>
+<header>
+    <a href=""><i class="leftArrow"><</i></a>
+    <h1>ToDo Detailansicht</h1>
+</header>
+<body>
+<hr>
+<div class="detailtodo">
+    <label for="titel">Titel:</label>
+    <p>Beispiel Titel</p>
+    <?php
+
+    ?>
+    <hr>
+    <br>
+    <label for="dauer">Dauer:</label>
+    <?php
+
+    ?>
+    <hr>
+    <br>
+    <label for="deadline">Deadline:</label>
+    <?php
+
+    ?>
+    <hr>
+    <br>
+    <label for="info">Info:</label>
+    <br>
+    <?php
+
+    ?>
+</div>
+</body>
+<footer>
+</footer>
+</html>
