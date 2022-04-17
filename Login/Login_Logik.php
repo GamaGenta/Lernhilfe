bfbbb5b723643c44ca8534ce029ffa2be8dfef65
Gama <94679178+GamaGenta@users.noreply.github.com>
2022-04-17 22:11:51 +0200

    Add files via upload

--- Login/Login_Logik.php
+++ Login/Login_Logik.php
@@ -33,18 +33,8 @@ function sucheRolle ($uid, $password) {
     return $rolle;
 }
 
-/*
-function setSessionVars ($uid, $rolle) {
-    $_SESSION["uid"] = $uid;
-    $_SESSION["rolle"] = $rolle;
-}
-*/
 
 function getUserClassFrom($uid, $password){
-    //Authentifizierung der User daten: gibt User Rolle zur Validierung zurück
-    // PHP Fehlermeldungen anzeigen
-    //error_reporting(E_ALL);
-    //ini_set('display_errors', true);
 
 // Zugangsdaten zur Datenbank
     $DB_HOST = "localhost"; // Host-Adresse
@@ -53,34 +43,18 @@ function getUserClassFrom($uid, $password){
     $DB_PASSWORT = ""; // Passwort
 
 
-    $OPTION = [
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4",
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ];
-
-    //$result = null;
     try {
         // Verbindung zur Datenbank aufbauen
-        //$db = new PDO("mysql:host=" . $DB_HOST . ";dbname=" . $DB_NAME, $DB_BENUTZER, $DB_PASSWORT, $OPTION);
         $conn = mysqli_connect($DB_HOST, $DB_BENUTZER, $DB_PASSWORT, $DB_NAME);
         $sqlSelect = "SELECT Rolle FROM thmmitglied WHERE Username = '$uid' and Passwort = '$password';";
         $result = mysqli_query($conn, $sqlSelect);
-        //$resultCheck = mysqli_num_rows($result);
     }
     catch (PDOException $e) {
         // Bei einer fehlerhaften Verbindung eine Nachricht ausgeben
         exit("Verbindung fehlgeschlagen! " . $e->getMessage());
     }
-/*
-    $dbServername = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbName = "mib14test";
 
-    $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-*/
     error_reporting(0);
     return $result->fetch_row()[0];
-    //return "Student";
 }
 
